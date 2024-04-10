cf8d7d0500c33d150c8745712d957d389b711370
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-04-10 14:09:02 +0100

    Changed next-js config to use process.env

--- app/(landing)/header.tsx
+++ app/(landing)/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
             <div className="lg:max-w-screen-xl mx-auto flex items-center justify-between h-full">
                 <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-                    <Image src="./monkey.svg" height={75} width={75} alt="Monkey"></Image>
+                    <Image src="monkey.svg" height={75} width={75} alt="Monkey"></Image>
                     <h1 className="text-2xl font-extrabold text-dark-blue tracking-wide">My Money Confidence</h1>
                 </div>
             </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const isProductive = process.env.NODE_ENV == 'production';
+//const isProductive = process.env.NODE_ENV == 'production';
 
 const nextConfig = {
-  basePath: isProductive ? '/financial-lit' : '',
+  //basePath: isProductive ? '/financial-lit' : '',
   output: 'export',
+  assetPrefix: process.env.assetPrefix,
+  basePath: process.env.basePath,
   images: {
     unoptimized: true,
   },
