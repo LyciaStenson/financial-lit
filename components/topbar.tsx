a2a8e38c2aa224e80e7d48678e2a57f3a066326c
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-05-15 16:35:18 +0100

    Switched TopbarItem to TopBarButton

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -1,4 +1,4 @@
-import { TopbarItem } from "./topbar-button";
+import { TopBarButton } from "./topbar-button";
 import { Rankbar } from "./rankbar";
 import { Points } from "./points";
 import { StreakIcon } from "./streak-icon";
@@ -7,7 +7,7 @@ export const Topbar = () => {
     return (
         <div className="w-full h-[90px] bg-white fixed z-50">
             <div className={"gap-[48px] max-w-[850px] mx-auto flex items-center justify-center space-x-4 top-0"}>
-            <TopbarItem
+            <TopBarButton
                 alt="Home"
                 iconSrc="./astronaut-peace.svg"
                 href="/home"
