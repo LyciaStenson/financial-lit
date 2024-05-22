578ac699df43a366332a22ba6b70c7b22560e190
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-05-22 12:55:19 +0100

    Added padding around home page banner

--- app/(game)/home/banner.tsx
+++ app/(game)/home/banner.tsx
@@ -8,15 +8,18 @@ export const Banner = ({
     description,
 }: Props) => {
     return (
-        <div className="flex max-w-[450px] rounded-2xl justify-center text-center border-2 border-b-8 stripes stripes-size-[300px] stripes-opacity-30 stripes-white bg-moneyconf-gold py-2 text-moneyconf-purple border-moneyconf-purple">
-            <div className="space-y-1 z-10">
-                <h1 className="text-lg font-extrabold">
-                    {title}
-                </h1>
-                <h2 className="text-xl font-extrabold">
-                    {description}
-                </h2>
+        <div className="flex w-full px-5 items-center justify-center">
+            <div className="rounded-2xl text-center border-2 border-b-8 stripes stripes-size-[300px] stripes-opacity-30 stripes-white bg-moneyconf-gold py-2 text-moneyconf-purple border-moneyconf-purple">
+                <div className="space-y-1 z-10">
+                    <h1 className="text-lg font-extrabold">
+                        {title}
+                    </h1>
+                    <h2 className="text-xl font-extrabold">
+                        {description}
+                    </h2>
+                </div>
             </div>
         </div>
+
     )
 }
